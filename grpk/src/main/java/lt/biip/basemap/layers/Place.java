17c6ae4af313aabad2a8efe9fccce926269459ed
Karolis Vyčius <k.vycius@gmail.com>
2024-04-05 12:49:55 +0300

    Ignore ANTR

--- grpk/src/main/java/lt/biip/basemap/layers/Place.java
+++ grpk/src/main/java/lt/biip/basemap/layers/Place.java
@@ -14,14 +14,17 @@ public class Place implements ForwardingProfile.FeatureProcessor {
         if (sf.getSource().equals("grpk") && sf.getSourceLayer().equals("VIETOV_T") && sf.isPoint()) {
             var code = sf.getString("GKODAS");
 
-            switch (code) {
-                case "uas1" -> addFeature("country", 0, sf, features);
-                case "uas2" -> addFeature("province", 4, sf, features);
-                case "uas511" -> addFeature("city", 6, sf, features);
-                case "uas512" -> addFeature("town", 11, sf, features);
-                case "uas52" -> addFeature("village", 12, sf, features);
-                case "uas53" -> addFeature("suburb", 13, sf, features);
-                case "uas54" -> addFeature("isolated_dwelling", 14, sf, features);
+            // TODO: add ANTR at higher zoom levels
+            if (sf.getString("ANTR") == null) {
+                switch (code) {
+                    case "uas1" -> addFeature("country", 0, sf, features);
+                    case "uas2" -> addFeature("province", 4, sf, features);
+                    case "uas511" -> addFeature("city", 6, sf, features);
+                    case "uas512" -> addFeature("town", 11, sf, features);
+                    case "uas52" -> addFeature("village", 12, sf, features);
+                    case "uas53" -> addFeature("suburb", 13, sf, features);
+                    case "uas54" -> addFeature("isolated_dwelling", 14, sf, features);
+                }
             }
         }
     }
