afaa5f65bc44b24b97da38fc55521414048c7530
Karolis Vyčius <k.vycius@gmail.com>
2024-04-06 13:34:33 +0300

    Arrodrome labels

--- grpk/src/main/java/lt/biip/basemap/layers/Aeroway.java
+++ grpk/src/main/java/lt/biip/basemap/layers/Aeroway.java
@@ -1,15 +1,10 @@
 package lt.biip.basemap.layers;
 
 import com.onthegomap.planetiler.FeatureCollector;
-import com.onthegomap.planetiler.FeatureMerge;
 import com.onthegomap.planetiler.ForwardingProfile;
-import com.onthegomap.planetiler.VectorTile;
-import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SourceFeature;
 
-import java.util.List;
-
-public class Aeroway implements ForwardingProfile.FeaturePostProcessor, ForwardingProfile.FeatureProcessor {
+public class Aeroway implements ForwardingProfile.FeatureProcessor {
 
     @Override
     public void processFeature(SourceFeature sf, FeatureCollector features) {
@@ -26,29 +21,9 @@ public class Aeroway implements ForwardingProfile.FeaturePostProcessor, Forwardi
 
 
     public void addFeature(String attrClass, SourceFeature sf, FeatureCollector features) {
-        features.polygon(this.name())
+        features.polygon("aeroway")
                 .setAttr("class", attrClass)
-                .setMinZoom(10);
-
-        var name = sf.getString("VARDAS");
-        if (name != null) {
-            features.centroid("aerodrome_label")
-                    .setAttr("name", name)
-                    .setMinZoom(10);
-        }
-    }
-
-    @Override
-    public List<VectorTile.Feature> postProcess(int zoom, List<VectorTile.Feature> items) throws GeometryException {
-        if (zoom >= 14) {
-            return items;
-        }
-
-        return FeatureMerge.mergeNearbyPolygons(items, 3.125, 3.125, 0.5, 0.5);
-    }
-
-    @Override
-    public String name() {
-        return "aeroway";
+                .setMinZoom(10)
+                .setMinPixelSize(2);
     }
 }
