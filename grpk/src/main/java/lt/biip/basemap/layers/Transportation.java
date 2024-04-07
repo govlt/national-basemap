21a3d1c26b77b68a93b5cec486e8dcb35c268280
Karolis Vyčius <k.vycius@gmail.com>
2024-04-07 12:45:12 +0300

    Extract field and values into static class

--- grpk/src/main/java/lt/biip/basemap/layers/Transportation.java
+++ grpk/src/main/java/lt/biip/basemap/layers/Transportation.java
@@ -17,25 +17,6 @@ public class Transportation implements ForwardingProfile.FeaturePostProcessor, F
 
     static final List<String> PAVED_VALUES = Arrays.asList("A", "C", "G", "Md");
 
-    static final String FIELD_CLASS = "class";
-    static final String FIELD_EXPRESSWAY = "expressway";
-    static final String FIELD_REF = "ref";
-    static final String FIELD_REF_LENGTH = "ref_length";
-    static final String FIELD_LEVEL = "level";
-    static final String FIELD_SURFACE = "level";
-
-    public static final String CLASS_MOTORWAY = "motorway";
-    public static final String CLASS_TRUNK = "trunk";
-    public static final String CLASS_PRIMARY = "primary";
-    public static final String CLASS_SECONDARY = "secondary";
-    public static final String CLASS_TERTIARY = "tertiary";
-    public static final String CLASS_RESIDENTIAL = "residential";
-    public static final String CLASS_LINK = "link";
-    public static final String CLASS_SERVICE = "service";
-    public static final String CLASS_PATH = "path";
-    public static final String CLASS_FERRY = "ferry";
-    public static final String CLASS_UNCLASSIFIED = "unclassified";
-    public static final String CLASS_TRACK = "track";
 
     @Override
     public void processFeature(SourceFeature sf, FeatureCollector features) {
@@ -46,40 +27,40 @@ public class Transportation implements ForwardingProfile.FeaturePostProcessor, F
                 var danga = sf.getString("DANGA");
 
                 if (tipas == 1) {
-                    addTransportationFeature(CLASS_MOTORWAY, 2, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_MOTORWAY, 2, sf, features);
                 } else if (tipas == 5) {
-                    addTransportationFeature(CLASS_TRUNK, 4, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_TRUNK, 4, sf, features);
                 } else if (tipas == 2) {
-                    addTransportationFeature(CLASS_PRIMARY, 4, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_PRIMARY, 4, sf, features);
                 } else if (tipas == 3) {
-                    addTransportationFeature(CLASS_SECONDARY, 8, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_SECONDARY, 8, sf, features);
                 } else if (tipas == 4) {
-                    addTransportationFeature(CLASS_TERTIARY, 8, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_TERTIARY, 8, sf, features);
                 } else if (tipas == 6) {
-                    addTransportationFeature(CLASS_RESIDENTIAL, 12, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_RESIDENTIAL, 12, sf, features);
                 } else if (tipas == 7 && paskirtis.equals("JUNG")) {
-                    addTransportationFeature(CLASS_LINK, 13, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_LINK, 13, sf, features);
                 } else if (tipas == 7 || tipas == 9) {
-                    addTransportationFeature(CLASS_SERVICE, 13, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_SERVICE, 13, sf, features);
                 } else if (tipas == 8 && danga.equals("Ž")) {
-                    addTransportationFeature(CLASS_PATH, 14, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_PATH, 14, sf, features);
                 } else if (tipas == 8) {
-                    addTransportationFeature(CLASS_SERVICE, 13, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_SERVICE, 13, sf, features);
                 } else if (tipas == 10 || tipas == 11 || tipas == 13) {
-                    addTransportationFeature(CLASS_PATH, 14, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_PATH, 14, sf, features);
                 } else if (tipas == 14) {
-                    addTransportationFeature(CLASS_FERRY, 13, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_FERRY, 13, sf, features);
                 } else {
-                    addTransportationFeature(CLASS_UNCLASSIFIED, 14, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_UNCLASSIFIED, 14, sf, features);
                 }
             } else if (sf.getSourceLayer().equals("GELEZINK")) {
                 var gkodas = sf.getString("GKODAS");
                 var tipas = sf.getLong("TIPAS");
 
                 if (Arrays.asList("gz1", "gz2", "gz1gz2").contains(gkodas) && tipas == 1) {
-                    addTransportationFeature(CLASS_TRACK, 11, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_TRACK, 11, sf, features);
                 } else {
-                    addTransportationFeature(CLASS_TRACK, 8, sf, features);
+                    addTransportationFeature(FieldValue.CLASS_TRACK, 8, sf, features);
                 }
             }
         }
@@ -89,17 +70,17 @@ public class Transportation implements ForwardingProfile.FeaturePostProcessor, F
         var level = (int) sf.getLong("LYGMUO");
         var name = nullIfEmpty(sf.getString("VARDAS"));
 
-        var expressway = clazz.equals(CLASS_MOTORWAY);
+        var expressway = clazz.equals(FieldValue.CLASS_MOTORWAY);
 
         var ref = expressway ? nullIfEmpty(sf.getString("NUMERIS")) : null;
         var refLength = ref != null ? ref.length() : null;
         var surface = PAVED_VALUES.contains(sf.getString("DANGA")) ? "paved" : "unpaved";
 
         features.line(this.name())
-                .setAttr(FIELD_CLASS, clazz)
-                .setAttr(FIELD_EXPRESSWAY, expressway)
-                .setAttr(FIELD_LEVEL, level)
-                .setAttr(FIELD_SURFACE, surface)
+                .setAttr(Field.CLASS, clazz)
+                .setAttr(Field.EXPRESSWAY, expressway)
+                .setAttr(Field.LEVEL, level)
+                .setAttr(Field.SURFACE, surface)
                 .setAttr("minZoom", minZoom)
                 .setMinZoom(minZoom)
                 .setMinPixelSize(0.0)
@@ -108,10 +89,10 @@ public class Transportation implements ForwardingProfile.FeaturePostProcessor, F
         if (ref != null || name != null) {
             features.line("transportation_name")
                     .putAttrs(LanguageUtils.getNames(sf.tags()))
-                    .setAttr(FIELD_CLASS, clazz)
-                    .setAttr(FIELD_REF, ref)
-                    .setAttr(FIELD_REF_LENGTH, refLength)
-                    .setAttr(FIELD_LEVEL, level)
+                    .setAttr(Field.CLASS, clazz)
+                    .setAttr(Field.REF, ref)
+                    .setAttr(Field.REF_LENGTH, refLength)
+                    .setAttr(Field.LEVEL, level)
                     .setMinPixelSize(0.0)
                     .setPixelTolerance(0.0)
                     .setMinZoom(Math.min(minZoom + 2, 14));
@@ -135,5 +116,29 @@ public class Transportation implements ForwardingProfile.FeaturePostProcessor, F
     @Override
     public String name() {
         return "transportation";
+    }
+
+    private static final class Field {
+        static final String CLASS = "class";
+        static final String EXPRESSWAY = "expressway";
+        static final String REF = "ref";
+        static final String REF_LENGTH = "ref_length";
+        static final String LEVEL = "level";
+        static final String SURFACE = "level";
+    }
+
+    private static class FieldValue {
+        static final String CLASS_MOTORWAY = "motorway";
+        static final String CLASS_TRUNK = "trunk";
+        static final String CLASS_PRIMARY = "primary";
+        static final String CLASS_SECONDARY = "secondary";
+        static final String CLASS_TERTIARY = "tertiary";
+        static final String CLASS_RESIDENTIAL = "residential";
+        static final String CLASS_LINK = "link";
+        static final String CLASS_SERVICE = "service";
+        static final String CLASS_PATH = "path";
+        static final String CLASS_FERRY = "ferry";
+        static final String CLASS_UNCLASSIFIED = "unclassified";
+        static final String CLASS_TRACK = "track";
     }
 }
