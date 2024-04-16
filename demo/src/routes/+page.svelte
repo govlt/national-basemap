34f365b94d70d1c56f7034d0b690aa10300ba054
Karolis Vyčius <k.vycius@gmail.com>
2024-04-16 15:26:26 +0300

    Change to style url

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { MapLibre } from 'svelte-maplibre';
 
-	export let styleBaseUrl = new URL('vector/styles/bright/style.json', import.meta.env.VITE_STYLE_BASE_URL).href;
+	export let styleUrl = new URL('vector/styles/bright/style.json', import.meta.env.VITE_STYLE_BASE_URL).href;
 </script>
 
 <MapLibre
 	class="map"
 	standardControls
-	style="{ styleBaseUrl }">
+	style="{ styleUrl }">
 </MapLibre>
 
 <style>
