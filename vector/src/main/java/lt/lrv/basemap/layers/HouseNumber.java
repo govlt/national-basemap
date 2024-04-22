c0d785811b7c2b34a08e098650c090589134e8e3
Karolis Vyčius <k.vycius@gmail.com>
2024-04-22 16:51:08 +0300

    Merge house numbers with same attributes into multi points

--- vector/src/main/java/lt/lrv/basemap/layers/HouseNumber.java
+++ vector/src/main/java/lt/lrv/basemap/layers/HouseNumber.java
@@ -2,11 +2,17 @@ package lt.lrv.basemap.layers;
 
 
 import com.onthegomap.planetiler.FeatureCollector;
+import com.onthegomap.planetiler.FeatureMerge;
+import com.onthegomap.planetiler.ForwardingProfile;
+import com.onthegomap.planetiler.VectorTile;
+import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import lt.lrv.basemap.constants.Source;
 import lt.lrv.basemap.openmaptiles.OpenMapTilesSchema;
 
-public class HouseNumber implements OpenMapTilesSchema.Housenumber {
+import java.util.List;
+
+public class HouseNumber implements OpenMapTilesSchema.Housenumber, ForwardingProfile.FeaturePostProcessor {
 
     @Override
     public void processFeature(SourceFeature sf, FeatureCollector features) {
@@ -16,5 +22,10 @@ public class HouseNumber implements OpenMapTilesSchema.Housenumber {
                     .setAttr(Fields.HOUSENUMBER, sf.getTag("NR"))
                     .setMinZoom(14);
         }
+    }
+
+    @Override
+    public List<VectorTile.Feature> postProcess(int i, List<VectorTile.Feature> items) {
+        return FeatureMerge.mergeMultiPoint(items);
     }
 }
