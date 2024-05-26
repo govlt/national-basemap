5cf0565ba7cb794caf9307295d1bf113789fd942
Karolis Vyčius <k.vycius@gmail.com>
2024-05-26 09:41:12 +0300

    Avoid nested ifs

--- vector/src/main/java/lt/lrv/basemap/layers/Place.java
+++ vector/src/main/java/lt/lrv/basemap/layers/Place.java
@@ -10,40 +10,41 @@ import lt.lrv.basemap.utils.LanguageUtils;
 import java.util.List;
 import java.util.Objects;
 
+import static com.google.common.base.Strings.emptyToNull;
+
 public class Place implements OpenMapTilesSchema.Place {
 
     @Override
     public void processFeature(SourceFeature sf, FeatureCollector features) {
-        if (sf.getSource().equals(Source.GRPK) && sf.getSourceLayer().equals(Layers.GRPK_VIETOV_T) && sf.isPoint()) {
+        if (sf.getSource().equals(Source.GRPK) &&
+                sf.getSourceLayer().equals(Layers.GRPK_VIETOV_T) &&
+                sf.isPoint() &&
+                emptyToNull(sf.getString("ANTR")) == null
+        ) {
             var code = sf.getString("GKODAS");
             var adm_type = sf.getString("ADM_TIP");
             var pop = sf.getLong("GYVSK");
 
-            // TODO: add ANTR at higher zoom levels
-            if (sf.getString("ANTR", "").isEmpty()) {
-
-                if (Objects.equals(code, "uas2")) {
-                    addFeature(FieldValues.CLASS_PROVINCE, 4, sf, features);
-                } else if (Objects.equals(code, "uas511") && List.of("SOST", "APSK").contains(adm_type)) {
-                    addFeature(FieldValues.CLASS_CITY, 6, sf, features);
-                } else if (Objects.equals(code, "uas511") && (Objects.equals(adm_type, "SAV"))) {
-                    addFeature(FieldValues.CLASS_CITY, 7, sf, features);
-                } else if (Objects.equals(code, "uas511")) {
-                    addFeature(FieldValues.CLASS_TOWN, 8, sf, features);
-                } else if (Objects.equals(code, "uas512"))  {
-                    addFeature(FieldValues.CLASS_TOWN, 9, sf, features);
-                } else if (Objects.equals(code, "uas52") && pop > 200) {
-                    addFeature(FieldValues.CLASS_VILLAGE, 10, sf, features);
-                } else if (Objects.equals(code, "uas52") && pop > 50) {
-                    addFeature(FieldValues.CLASS_VILLAGE, 11, sf, features);
-                } else if (Objects.equals(code, "uas52")) {
-                    addFeature(FieldValues.CLASS_VILLAGE, 12, sf, features);
-                } else if (Objects.equals(code, "uas53")) {
-                    addFeature(FieldValues.CLASS_SUBURB, 11, sf, features);
-                } else if (Objects.equals(code, "uas54")) {
-                    addFeature(FieldValues.CLASS_ISOLATED_DWELLING, 14, sf, features);
-                }
-
+            if (Objects.equals(code, "uas2")) {
+                addFeature(FieldValues.CLASS_PROVINCE, 4, sf, features);
+            } else if (Objects.equals(code, "uas511") && List.of("SOST", "APSK").contains(adm_type)) {
+                addFeature(FieldValues.CLASS_CITY, 6, sf, features);
+            } else if (Objects.equals(code, "uas511") && (Objects.equals(adm_type, "SAV"))) {
+                addFeature(FieldValues.CLASS_CITY, 7, sf, features);
+            } else if (Objects.equals(code, "uas511")) {
+                addFeature(FieldValues.CLASS_TOWN, 8, sf, features);
+            } else if (Objects.equals(code, "uas512")) {
+                addFeature(FieldValues.CLASS_TOWN, 9, sf, features);
+            } else if (Objects.equals(code, "uas52") && pop > 200) {
+                addFeature(FieldValues.CLASS_VILLAGE, 10, sf, features);
+            } else if (Objects.equals(code, "uas52") && pop > 50) {
+                addFeature(FieldValues.CLASS_VILLAGE, 11, sf, features);
+            } else if (Objects.equals(code, "uas52")) {
+                addFeature(FieldValues.CLASS_VILLAGE, 12, sf, features);
+            } else if (Objects.equals(code, "uas53")) {
+                addFeature(FieldValues.CLASS_SUBURB, 11, sf, features);
+            } else if (Objects.equals(code, "uas54")) {
+                addFeature(FieldValues.CLASS_ISOLATED_DWELLING, 14, sf, features);
             }
         }
     }
