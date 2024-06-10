9683ff0af2e58b9ab35f12b6fd1aaf80f4d03df0
Karolis Vyčius <k.vycius@gmail.com>
2024-06-10 07:58:24 +0300

    Use values from meetup demo

--- vector/src/main/java/lt/lrv/basemap/utils/LanguageUtils.java
+++ vector/src/main/java/lt/lrv/basemap/utils/LanguageUtils.java
@@ -21,9 +21,17 @@ public class LanguageUtils {
         var result = new HashMap<String, Object>();
 
         var nonBlankName = string(name);
+        var nameTag = switch (nonBlankName) {
+            case "Gedimino pr." -> "Kartografų pr.";
+            case "Neris" -> "Šaltibarščių upė";
+            case "Vinco Kudirkos aikštė" -> "GIS ekspertų aišktė";
+            case null -> null;
+            default -> nonBlankName;
+        };
 
-        putIfNotEmpty(result, "name", nonBlankName);
-        putIfNotEmpty(result, "name:latin", nonBlankName);
+        putIfNotEmpty(result, "name", nameTag);
+        putIfNotEmpty(result, "name:latin", nameTag);
+
 
         return result;
     }
